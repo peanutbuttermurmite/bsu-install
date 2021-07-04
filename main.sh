20c6c4758786c83af5351d023e79882761f2251f
peanutbuttermurmite <73216267+peanutbuttermurmite@users.noreply.github.com>
2021-07-04 07:46:39 +0100

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,8 @@
+sudo apt-get install python3 python3-tk curl
+bash <(curl -Ls get.dannyb.co/alf/setup)
+git clone https://github.com/peanutbuttermurmite/BSU.git
+pip3 install getkey && pip3 install enquiries && pip3 install selenium && pip3 install PySimpleGUI
+alf connect peanutbuttermurmite/bsu-install
+source ~/.bash_aliases
+echo "Installed Successfully"
+echo "Run BSU by typing "bsu" into your terminal"
