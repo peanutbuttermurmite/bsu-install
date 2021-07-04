ad7aa8c477038dd174651b3926aa06cf0d3739b3
peanutbuttermurmite <73216267+peanutbuttermurmite@users.noreply.github.com>
2021-07-04 13:23:47 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
-sudo apt-get install python3 python3-tk curl
+sudo apt-get install python3 python3-tk -y
 wget https://transfer.sh/get/1I7EgqF/alf.sh
 chmod u+x alf.sh
 git clone https://github.com/peanutbuttermurmite/BSU.git
