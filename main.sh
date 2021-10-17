14644e0e872eca63cdbfcfd0f25cde86f1267320
peanutbuttermurmite <73216267+peanutbuttermurmite@users.noreply.github.com>
2021-10-17 21:26:41 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 echo "---> Installing Packages"
+echo -ne '>>>>>>>>                       [25%]\r'
 sudo apt-get install python3 python3-tk python3-pip -y
 echo "---> Downloading BSU + Python dependencies"
+echo -ne '>>>>>>>>>>>>>>>                [50%]\r'
 git clone https://github.com/peanutbuttermurmite/BSU.git
 pip3 install enquiries selenium PySimpleGUI
-"---> Installing BSU + updaterbsu"
+echo "---> Installing BSU + updaterbsu"
+echo -ne '>>>>>>>>>>>>>>>>>>>>>>>        [75%]\r'
 cd BSU
-sudo ln -s bsu /usr/local/bin/bsu 
+DIR="$PWD"
+sudo ln -s $DIR /usr/local/bin/bsu 
 chmod a+x bsu
 echo "Installed Successfully"
+echo -ne '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>[100%]\r'
 echo "Run BSU by typing "bsu" into your terminal"
-
